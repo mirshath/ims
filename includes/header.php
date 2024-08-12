91c69acd70a07a4cd9383a1c6811d8a9166d4d8d
Mirshath Moahmed <100135478+mirshath@users.noreply.github.com>
2024-08-12 16:56:11 +0530

    Done

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Zit Admin 2 - Dashboard</title>
+    <title>Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -19,6 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
--- nav.php
+++ nav.php
@@ -1,90 +1,109 @@
  <!-- Sidebar -->
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-    <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-laugh-wink"></i>
-    </div>
-    <div class="sidebar-brand-text mx-3">Zit Admin <sup>2</sup></div>
-</a>
+     <!-- Sidebar - Brand -->
+     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+         <div class="sidebar-brand-icon rotate-n-15">
+             <i class="fas fa-laugh-wink"></i>
+         </div>
+         <div class="sidebar-brand-text mx-3">IMS <sup>2</sup></div>
+     </a>
 
-<!-- Divider -->
-<hr class="sidebar-divider my-0">
+     <!-- Divider -->
+     <hr class="sidebar-divider my-0">
 
-<!-- Nav Item - Dashboard -->
-<li class="nav-item active">
-    <a class="nav-link" href="index.php">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
-</li>
+     <!-- Nav Item - Dashboard -->
+     <li class="nav-item active">
+         <a class="nav-link" href="index.php">
+             <i class="fas fa-fw fa-tachometer-alt"></i>
+             <span>Dashboard</span></a>
+     </li>
 
-<!-- Divider -->
-<hr class="sidebar-divider">
+     <!-- Divider -->
+     <!-- <hr class="sidebar-divider"> -->
 
-<!-- Heading -->
-<div class="sidebar-heading">
-    Interface
-</div>
+     <!-- Heading -->
+     <!-- <div class="sidebar-heading">
+         Interface
+     </div> -->
 
-<!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-        aria-expanded="true" aria-controls="collapseTwo">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
-    </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.php">Buttons</a>
-            <a class="collapse-item" href="cards.php">Cards</a>
-        </div>
-    </div>
-</li>
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne"
+             aria-expanded="true" aria-controls="collapseOne">
+             <i class="fas fa-fw fa-cog"></i>
+             <span>Master File</span>
+         </a>
+         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">MAster file > Components</h6>
+                 <a class="collapse-item" href="m.php">Lead Types</a>
+                 <a class="collapse-item" href="">Year</a>
+                 <a class="collapse-item" href="">Semester</a>
+                 <a class="collapse-item" href="">Criteria</a>
+                 <a class="collapse-item" href="">University</a>
+                 <a class="collapse-item" href="">Cooordinator</a>
+                 <a class="collapse-item" href="">Program</a>
+                 <a class="collapse-item" href="">Lecture</a>
+                 <a class="collapse-item" href="">Module</a>
+                 <a class="collapse-item" href="">Batch</a>
+                 <a class="collapse-item" href="">Grade</a>
+                 <a class="collapse-item" href="">Currency</a>
+                 <a class="collapse-item" href="">Status</a>
+                 <a class="collapse-item" href="">Decision</a>
+             </div>
+         </div>
+     </li>
 
-<!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-        aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.php">Colors</a>
-            <a class="collapse-item" href="utilities-border.php">Borders</a>
-            <a class="collapse-item" href="utilities-animation.php">Animations</a>
-            <a class="collapse-item" href="utilities-other.php">Other</a>
-        </div>
-    </div>
-</li>
-
-<!-- Divider -->
-<hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-    Addons
-</div>
+     <!-- Divider -->
+     <!-- <hr class="sidebar-divider"> -->
 
 
 
-<!-- Nav Item - Tables -->
-<li class="nav-item">
-    <a class="nav-link" href="tables.php">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span></a>
-</li>
+     <li class="nav-item">
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+             aria-expanded="true" aria-controls="collapseTwo">
+             <i class="fas fa-fw fa-cog"></i>
+             <span>Transections</span>
+         </a>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+             <div class="bg-white py-2 collapse-inner rounded">
+                 <h6 class="collapse-header">Master file > Components</h6>
+                 <a class="collapse-item" href="#">Lead Types</a>
+                 <a class="collapse-item" href="#">Year</a>
+                 <a class="collapse-item" href="#">Semester</a>
+                 <a class="collapse-item" href="#">Criteria</a>
+                 <a class="collapse-item" href="#">University</a>
+                 <a class="collapse-item" href="#">Cooordinator</a>
+                 <a class="collapse-item" href="#">Program</a>
+                 <a class="collapse-item" href="#">Lecture</a>
+                 <a class="collapse-item" href="#">Module</a>
+                 <a class="collapse-item" href="#">Batch</a>
+                 <a class="collapse-item" href="#">Grade</a>
+                 <a class="collapse-item" href="#">Currency</a>
+                 <a class="collapse-item" href="#">Status</a>
+                 <a class="collapse-item" href="#">Decision</a>
+             </div>
+         </div>
+     </li>
 
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
 
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-</div>
-</ul>
-<!-- End of Sidebar -->
+
+
+
+     <!-- Nav Item - Tables -->
+     <li class="nav-item">
+         <a class="nav-link" href="tables.php">
+             <i class="fas fa-fw fa-table"></i>
+             <span>Tables</span></a>
+     </li>
+
+     <!-- Divider -->
+     <!-- <hr class="sidebar-divider d-none d-md-block"> -->
+
+     <!-- Sidebar Toggler (Sidebar) -->
+     <div class="text-center d-none d-md-inline">
+         <button class="rounded-circle border-0" id="sidebarToggle"></button>
+     </div>
+ </ul>
+ <!-- End of Sidebar -->
