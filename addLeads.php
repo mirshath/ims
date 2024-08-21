ac35d5a1e6a9aa40adee9dc7de4ce9ecf6b16936
Mirshath Moahmed <100135478+mirshath@users.noreply.github.com>
2024-08-21 16:44:08 +0530

    Done

--- addLeads.php
+++ addLeads.php
@@ -125,129 +125,168 @@ if (isset($_GET['delete'])) {
                 <form action="addLeads.php" method="POST" class="mt-4">
                     <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
 
-                    <!-- Form fields for lead details -->
-                    <div class="form-group">
-                        <label for="date">Date:</label>
-                        <input type="date" name="date" class="form-control" value="<?php echo htmlspecialchars($date); ?>" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="type">Type:</label>
-                        <select class="form-control" id="type" name="type" required>
-                            <option value="" disabled>Select Type</option>
-                            <?php
-                            // Populate the dropdown with lead types
-                            if ($result_lead->num_rows > 0) {
-                                while ($row = $result_lead->fetch_assoc()) {
-                                    $selected = ($row['lead_type'] === $type) ? 'selected' : '';
-                                    echo '<option value="' . htmlspecialchars($row['lead_type']) . '" ' . $selected . '>' . htmlspecialchars($row['lead_type']) . '</option>';
-                                }
-                            } else {
-                                echo '<option value="" disabled>No types available</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-
-                    <!-- University Dropdown -->
-                    <div class="form-group">
-                        <label for="university">University:</label>
-                        <select name="university" id="university" class="form-control" required>
-                            <option value="" disabled selected>Select University</option>
-                            <?php
-                            if ($result_universities->num_rows > 0) {
-                                while ($row = $result_universities->fetch_assoc()) {
-                                    $selected = ($row['id'] == $university) ? 'selected' : '';
-                                    echo '<option value="' . htmlspecialchars($row['id']) . '" ' . $selected . '>' . htmlspecialchars($row['university_name']) . '</option>';
-                                }
-                            } else {
-                                echo '<option value="" disabled>No universities available</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-
-                    <!-- Programme Dropdown -->
-                    <div class="form-group">
-                        <label for="programme">Programme:</label>
-                        <select name="programme" id="programme" class="form-control" required>
-                            <option value="" disabled selected>Select Programme</option>
-                            <!-- Options will be populated dynamically via AJAX -->
-                        </select>
-                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="date">Date:</label>
+                                <input type="date" name="date" class="form-control" value="<?php echo htmlspecialchars($date); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="type">Type:</label>
+                                <select class="form-control" id="type" name="type" required>
+                                    <option value="" disabled>Select Type</option>
+                                    <?php
+                                    // Populate the dropdown with lead types
+                                    if ($result_lead->num_rows > 0) {
+                                        while ($row = $result_lead->fetch_assoc()) {
+                                            $selected = ($row['lead_type'] === $type) ? 'selected' : '';
+                                            echo '<option value="' . htmlspecialchars($row['lead_type']) . '" ' . $selected . '>' . htmlspecialchars($row['lead_type']) . '</option>';
+                                        }
+                                    } else {
+                                        echo '<option value="" disabled>No types available</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
 
 
-                    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                            <!-- University Dropdown -->
+                            <div class="form-group">
+                                <label for="university">University:</label>
+                                <select name="university" id="university" class="form-control" required>
+                                    <option value="" disabled selected>Select University</option>
+                                    <?php
+                                    if ($result_universities->num_rows > 0) {
+                                        while ($row = $result_universities->fetch_assoc()) {
+                                            $selected = ($row['id'] == $university) ? 'selected' : '';
+                                            echo '<option value="' . htmlspecialchars($row['id']) . '" ' . $selected . '>' . htmlspecialchars($row['university_name']) . '</option>';
+                                        }
+                                    } else {
+                                        echo '<option value="" disabled>No universities available</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
 
 
-                    <script>
-                        $(document).ready(function() {
-                            var selectedProgramme = '<?php echo $programme; ?>';
+                            <!-- Programme Dropdown -->
+                            <div class="form-group">
+                                <label for="programme">Programme:</label>
+                                <select name="programme" id="programme" class="form-control" required>
+                                    <option value="" disabled selected>Select Programme</option>
+                                    <!-- Options will be populated dynamically via AJAX -->
+                                </select>
+                            </div>
 
-                            $('#university').on('change', function() {
-                                var universityID = $(this).val();
-                                if (universityID) {
-                                    $.ajax({
-                                        type: 'POST',
-                                        url: 'get_programs.php',
-                                        data: {
-                                            university_id: universityID
-                                        },
-                                        dataType: 'json',
-                                        success: function(data) {
-                                            $('#programme').html('<option value="">Select Programme</option>');
-                                            $.each(data, function(key, value) {
-                                                var isSelected = value.program_name == selectedProgramme ? 'selected' : '';
-                                                $('#programme').append('<option value="' + value.program_name + '" ' + isSelected + '>' + value.program_name + '</option>');
+
+
+                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+                            <script>
+                                $(document).ready(function() {
+                                    var selectedProgramme = '<?php echo $programme; ?>';
+
+                                    $('#university').on('change', function() {
+                                        var universityID = $(this).val();
+                                        if (universityID) {
+                                            $.ajax({
+                                                type: 'POST',
+                                                url: 'get_programs.php',
+                                                data: {
+                                                    university_id: universityID
+                                                },
+                                                dataType: 'json',
+                                                success: function(data) {
+                                                    $('#programme').html('<option value="">Select Programme</option>');
+                                                    $.each(data, function(key, value) {
+                                                        var isSelected = value.program_name == selectedProgramme ? 'selected' : '';
+                                                        $('#programme').append('<option value="' + value.program_name + '" ' + isSelected + '>' + value.program_name + '</option>');
+                                                    });
+                                                }
                                             });
+                                        } else {
+                                            $('#programme').html('<option value="">Select Programme</option>');
                                         }
                                     });
-                                } else {
-                                    $('#programme').html('<option value="">Select Programme</option>');
-                                }
-                            });
 
-                            // Trigger change event to load programs if university is already selected (for edit mode)
-                            <?php if ($update && !empty($university)): ?>
-                                $('#university').trigger('change');
-                            <?php endif; ?>
-                        });
-                    </script>
+                                    // Trigger change event to load programs if university is already selected (for edit mode)
+                                    <?php if ($update && !empty($university)): ?>
+                                        $('#university').trigger('change');
+                                    <?php endif; ?>
+                                });
+                            </script>
 
-                    <div class="form-group">
-                        <label for="intake">Intake Date:</label>
-                        <input type="date" name="intake" class="form-control" value="<?php echo htmlspecialchars($intake); ?>" required>
+                        </div>
+                        <div class="col-md-6">
+
+
+                            <div class="form-group">
+                                <label for="intake">Intake Date:</label>
+                                <input type="date" name="intake" class="form-control" value="<?php echo htmlspecialchars($intake); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="first_name">First Name:</label>
+                                <input type="text" name="first_name" class="form-control" value="<?php echo htmlspecialchars($first_name); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+
+                            <div class="form-group">
+                                <label for="last_name">Last Name:</label>
+                                <input type="text" name="last_name" class="form-control" value="<?php echo htmlspecialchars($last_name); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="contact">Contact:</label>
+                                <input type="text" name="contact" class="form-control" value="<?php echo htmlspecialchars($contact); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" name="email" class="form-control" value="<?php echo htmlspecialchars($email); ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="details">Details:</label>
+                                <textarea name="details" class="form-control"><?php echo htmlspecialchars($details); ?></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="status">Status</label>
+                                <select class="form-control" id="status" name="status" required>
+                                    <option value="New" <?php echo ($status == 'New') ? 'selected' : ''; ?>>New</option>
+                                    <option value="Contacted" <?php echo ($status == 'Contacted') ? 'selected' : ''; ?>>Contacted</option>
+                                    <option value="Qualified" <?php echo ($status == 'Qualified') ? 'selected' : ''; ?>>Qualified</option>
+                                    <option value="Lost" <?php echo ($status == 'Lost') ? 'selected' : ''; ?>>Lost</option>
+                                    <option value="Converted" <?php echo ($status == 'Converted') ? 'selected' : ''; ?>>Converted</option>
+                                </select>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="form-group">
-                        <label for="first_name">First Name:</label>
-                        <input type="text" name="first_name" class="form-control" value="<?php echo htmlspecialchars($first_name); ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="last_name">Last Name:</label>
-                        <input type="text" name="last_name" class="form-control" value="<?php echo htmlspecialchars($last_name); ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="contact">Contact:</label>
-                        <input type="text" name="contact" class="form-control" value="<?php echo htmlspecialchars($contact); ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" name="email" class="form-control" value="<?php echo htmlspecialchars($email); ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="details">Details:</label>
-                        <textarea name="details" class="form-control"><?php echo htmlspecialchars($details); ?></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="status">Status</label>
-                        <select class="form-control" id="status" name="status" required>
-                            <option value="New" <?php echo ($status == 'New') ? 'selected' : ''; ?>>New</option>
-                            <option value="Contacted" <?php echo ($status == 'Contacted') ? 'selected' : ''; ?>>Contacted</option>
-                            <option value="Qualified" <?php echo ($status == 'Qualified') ? 'selected' : ''; ?>>Qualified</option>
-                            <option value="Lost" <?php echo ($status == 'Lost') ? 'selected' : ''; ?>>Lost</option>
-                            <option value="Converted" <?php echo ($status == 'Converted') ? 'selected' : ''; ?>>Converted</option>
-                        </select>
-                    </div>
+
+                    <!-- Form fields for lead details -->
+
+
 
 
                     <!-- Submit Button -->
@@ -262,16 +301,16 @@ if (isset($_GET['delete'])) {
                             <tr>
                                 <th>Date</th>
                                 <th>Type</th>
-                                <th>University</th>
+                                <!-- <th>University</th> -->
                                 <th>Programme</th>
-                                <th>Intake Date</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
+                                <th>Intake</th>
+                                <th>Name</th>
+                                <!-- <th>Last Name</th> -->
                                 <th>Contact</th>
                                 <th>Email</th>
                                 <th>Details</th>
                                 <th>Status</th>
-                                <th>Actions</th>
+                                <!-- <th>Actions</th> -->
                             </tr>
                         </thead>
                         <tbody>
@@ -283,11 +322,11 @@ if (isset($_GET['delete'])) {
                                 echo '<tr>';
                                 echo '<td>' . htmlspecialchars($row['date']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['type']) . '</td>';
-                                echo '<td>' . htmlspecialchars($row['university']) . '</td>';
+                                // echo '<td>' . htmlspecialchars($row['university']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['programme']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['intake']) . '</td>';
-                                echo '<td>' . htmlspecialchars($row['first_name']) . '</td>';
-                                echo '<td>' . htmlspecialchars($row['last_name']) . '</td>';
+                                echo '<td>' . htmlspecialchars($row['first_name']) . ' '. $row['last_name']. '</td>';
+                                // echo '<td>' . htmlspecialchars($row['last_name']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['contact']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['email']) . '</td>';
                                 echo '<td>' . htmlspecialchars($row['details']) . '</td>';
